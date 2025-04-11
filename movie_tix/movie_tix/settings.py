dc0b55fdd0e3b93bc18cd8c49f86c03a12385be7
Arsen <arsenzhanserkenov@gmail.com>
2025-04-11 11:06:37 +0500

    docker

--- movie_tix/movie_tix/settings.py
+++ movie_tix/movie_tix/settings.py
@@ -200,8 +200,8 @@ STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY', '')
 # 3. Start Redis server
 # 4. Start Celery worker: celery -A movie_tix worker -l info
 # If Celery/Redis is not available, the application will fall back to synchronous processing
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_BROKER_URL = 'redis://redis:6379/0'
+CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
